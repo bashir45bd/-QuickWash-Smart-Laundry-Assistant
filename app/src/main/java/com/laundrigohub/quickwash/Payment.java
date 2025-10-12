12f3bdfe22adf34e9c63835edf5600b7dc1f57b7
YourName <bashir45.me@gmail.com>
2025-10-12 22:13:01 +0600

    Commited

---
+++ app/src/main/java/com/laundrigohub/quickwash/Payment.java
@@ -0,0 +1,56 @@
+package com.laundrigohub.quickwash;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Payment extends AppCompatActivity {
+
+    private WebView webView;
+    private ProgressBar progressBar;
+    public static String POST_URL="";
+    ImageView payment_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_payment);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        CheckoutActivity.PAYMENT_PROCESS=true;
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+                if (newProgress == 50) progressBar.setVisibility(View.GONE);
+                else progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webView.postUrl("http://192.168.0.106/Laundry/sslpayment/checkout_hosted.php", POST_URL.getBytes());
+
+
+    }
+
+
+}
